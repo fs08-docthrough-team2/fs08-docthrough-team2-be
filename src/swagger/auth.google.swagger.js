cf04699df782231de29712c393f81ce5cbbc621b
MinJun916 <ed125248@gmail.com>
2025-11-10 01:26:19 +0900

    fix: YAMLSemanticError 에러 해결

--- src/swagger/auth.google.swagger.js
+++ src/swagger/auth.google.swagger.js
@@ -3,7 +3,9 @@
  * /api/auth/google/login:
  *   get:
  *     summary: Google 로그인 (Swagger에서는 실행되지 않습니다)
- *     description: Google OAuth 로그인 페이지로 이동합니다. Swagger에서는 실행되지 않으므로 브라우저에서 직접 테스트해야 합니다. 테스트 URL: http://localhost:3000/api/auth/google/login
+ *     description: >
+ *       Google OAuth 로그인 페이지로 이동합니다. Swagger에서는 실행되지 않으므로 브라우저에서 직접 테스트해야 합니다.
+ *       테스트 URL: http://localhost:3000/api/auth/google/login
  *     tags:
  *       - Auth
  *     responses:
@@ -16,7 +18,9 @@
  * /api/auth/google/callback:
  *   get:
  *     summary: Google 로그인 콜백 (Swagger에서는 실행되지 않습니다)
- *     description: Google 로그인 성공 후 호출되는 콜백 엔드포인트입니다. Swagger에서는 실행되지 않습니다. 브라우저에서 직접 테스트해야 합니다. 테스트 URL: http://localhost:3000/api/auth/google/callback
+ *     description: >
+ *       Google 로그인 성공 후 호출되는 콜백 엔드포인트입니다. Swagger에서는 실행되지 않습니다. 브라우저에서 직접 테스트해야 합니다.
+ *       테스트 URL: http://localhost:3000/api/auth/google/callback
  *     tags:
  *       - Auth
  *     responses:
