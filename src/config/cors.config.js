eaa55c91717b18fff019724b346abef5cb483dbc
cuckooolly <cuckooolly@gmail.com>
2025-11-10 14:06:48 +0900

    fix: 프론트엔드의 배포주소 CORS 설정 완료

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -5,7 +5,7 @@ const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.en
 dotenv.config({ path: envFile });
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://fs08-docthrough.onrender.com', 'https://fs08-docthrough-team2-fe.vercel.app/'], // 허용할 도메인
+  origin: ['http://localhost:3000', 'https://fs08-docthrough.onrender.com', 'https://fs08-docthrough-team2-fe.vercel.app/' ], // 허용할 도메인
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // 허용할 HTTP 메서드
   allowedHeaders: ['Content-Type', 'Authorization', 'x-study-password'], // 허용할 헤더
   credentials: true, // 쿠키 인증을 사용함
